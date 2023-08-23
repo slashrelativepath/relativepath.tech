ad9436a7fac7b37004cdb7e909ca947958483b00
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-23 10:48:14 -0500

    Added Header and Hero from template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
