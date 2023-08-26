e796d992f08e369b1e2a09db1be4ec4a483517da
christopherkeim <christopher.jl.keim@gmail.com>
2023-08-26 11:20:50 -0400

    Changed Hero image to relative path.

--- src/components/Hero/HeroRightCol.tsx
+++ src/components/Hero/HeroRightCol.tsx
@@ -1,7 +1,7 @@
 export function HeroRightCol() {
   return (
     <div className="w-full md:w-3/5 py-6 text-center">
-      <img className="w-full md:w-4/5 z-50" src="/hero.png" />
+      <img className="w-full md:w-4/5 z-50" src={`${import.meta.env.BASE_URL}hero.png`} />
     </div>
   );
 }
