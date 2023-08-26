20807766efc093d5f0d400e9ddcf680378e872b5
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-26 18:54:51 -0500

    Combined comment

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-# This script tests the site locally
+# This script tests if the site is running locally
 
-# Test that our server is running
 if [ $(curl -o /dev/null -s -w "%{http_code}" http://localhost:4173/ | grep 200) ]
 then
-  echo "Looking great! Fire up your browser and head over to http://localhost:5173"
+  echo "Server OK and running at http://localhost:5173"
 else
-  echo "Site is not up :( ." 
+  echo "Server not running or not OK" 
 fi
