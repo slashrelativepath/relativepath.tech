ae5a84ed093f4f9aa8f84cf05cd19374a5146d43
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-07 16:05:34 -0500

    Removed responsive behavior

--- src/partials/Header/Nav.tsx
+++ src/partials/Header/Nav.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export function Nav() {
   return (
     <nav
-      className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+      className="w-full flex-grow flex items-center mt-0 bg-white bg-transparent text-black p-4 z-20"
       id="nav-content"
     >
-      <ul className="list-reset lg:flex justify-end flex-1 items-center">
+      <ul className="list-reset flex justify-end flex-1 items-center">
         <Link to="/" className="inline-block py-2 px-4 text-black no-underline">
           Home
         </Link>
