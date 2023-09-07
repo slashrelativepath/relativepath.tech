3b8b8b6345b7a501ce6d94073481bcd128653b86
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-07 15:14:36 -0500

    Updated to router v6 pattern

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  *   to render a "not found" page when someone visits an unrecognized URL.
  */
 
-import { Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Bring in the layout
 import { Layout } from "./Layout";
@@ -13,23 +13,18 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { NoMatch } from "./pages/NoMatch";
 
-export default function App() {
-  return (
-    <>
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+// Create a router
+const router = createBrowserRouter([
+  {
+    Component: Layout,
+    children: [
+      { path: "/", Component: Home },
+      { path: "about", Component: About },
+      { path: "*", Component: NoMatch },
+    ],
+  },
+]);
 
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </>
-  );
+export default function App() {
+  return <RouterProvider router={router} />;
 }
