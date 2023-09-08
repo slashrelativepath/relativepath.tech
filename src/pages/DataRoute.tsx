d0a0ca68f8d36dc1b2969df408828bb95449a1f8
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-08 18:21:27 -0500

    Fixed as per linting rule

--- src/pages/DataRoute.tsx
+++ src/pages/DataRoute.tsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 
 export function DataRoute() {
-  let data = useLoaderData() as { message: string };
+  const data = useLoaderData() as { message: string };
   return <div>The data is: "{data.message}"</div>;
 }
