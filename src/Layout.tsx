38dbb07247c9c775d9ffbd67b4e5026eb4252cf9
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-24 16:34:19 -0400

    Added svg background decoration

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./partials/Header/Header";
+import "@fontsource-variable/inter";
+import PageIllustration from "./partials/PageIllistration";
 
 export function Layout() {
   return (
-    <div>
+    <div className="font-inter antialiased bg-gray-900 text-gray-200 tracking-tight min-h-screen">
       {/* A "layout route" is a good place to put markup you want to
             share across all the pages on your site, like navigation. */}
+      <PageIllustration />
       <Header />
 
       {/* An <Outlet> renders whatever child route is currently active,
