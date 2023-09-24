f2e0644bfb3574d622779010e4d7df3453ac44cd
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-24 16:35:58 -0400

    Set text to white

--- src/partials/Header/Nav.tsx
+++ src/partials/Header/Nav.tsx
@@ -3,29 +3,20 @@ import { Link } from "react-router-dom";
 export function Nav() {
   return (
     <nav
-      className="w-full flex-grow flex items-center mt-0 bg-white bg-transparent text-black p-4 z-20"
+      className="w-full flex-grow flex items-center mt-0 bg-transparent p-4 z-20"
       id="nav-content"
     >
       <ul className="list-reset flex justify-end flex-1 items-center">
-        <Link to="/" className="inline-block py-2 px-4 text-black no-underline">
+        <Link to="/" className="inline-block py-2 px-4  no-underline">
           Home
         </Link>
-        <Link
-          to="/about"
-          className="inline-block py-2 px-4 text-black no-underline"
-        >
+        <Link to="/about" className="inline-block py-2 px-4  no-underline">
           About
         </Link>
-        <Link
-          to="/data-route"
-          className="inline-block py-2 px-4 text-black no-underline"
-        >
+        <Link to="/data-route" className="inline-block py-2 px-4  no-underline">
           Data Route
         </Link>
-        <Link
-          to="/not-found"
-          className="inline-block py-2 px-4 text-black no-underline"
-        >
+        <Link to="/not-found" className="inline-block py-2 px-4  no-underline">
           Not Found
         </Link>
       </ul>
