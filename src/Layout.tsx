103e2d8481695d10fa02e306922eedadc620200b
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-24 22:58:10 -0400

    Fixed formatting for sticky footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,22 +2,24 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./partials/Header/Header";
 import "@fontsource-variable/inter";
 import PageIllustration from "./partials/PageIllistration";
+import Footer from "./partials/Footer";
 
+// A "layout route" is a good place to put markup you want to
+// share across all the pages on your site, like navigation.
 export function Layout() {
   return (
     <>
-      <main className="font-inter antialiased bg-gray-900 text-gray-200 tracking-tight min-h-screen">
-        {/* A "layout route" is a good place to put markup you want to
-            share across all the pages on your site, like navigation. */}
-        <PageIllustration />
+      <PageIllustration />
+      <div className="mx-auto flex min-h-screen max-w-6xl flex-col">
         <Header />
-
         {/* An <Outlet> renders whatever child route is currently active,
             so you can think about this <Outlet> as a placeholder for
             the child routes we defined above. */}
-        <Outlet />
-      </main>
-      {/* <Footer /> */}
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
