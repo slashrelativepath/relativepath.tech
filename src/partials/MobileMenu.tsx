2c046a13ca408c3aacf429873e78f1ea2e23e7ba
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-25 15:43:45 -0400

    Fixed responsiveness and links

--- src/partials/MobileMenu.tsx
+++ src/partials/MobileMenu.tsx
@@ -59,7 +59,7 @@ export function MobileMenu() {
       <nav
         id="mobile-nav"
         ref={mobileNav}
-        className="absolute left-0 top-0 z-20 w-full overflow-hidden pe-14 transition-all duration-300 ease-in-out sm:pe-20"
+        className="absolute right-0 top-20 z-20 w-fit overflow-hidden transition-all duration-300 ease-in-out"
         style={
           mobileNavOpen
             ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 }
@@ -69,88 +69,29 @@ export function MobileMenu() {
         <ul className="bg-gray-800 px-4 py-2">
           <li>
             <Link
-              to="/features"
-              className="flex py-2 text-gray-300 hover:text-gray-200"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Features
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/pricing"
-              className="flex py-2 text-gray-300 hover:text-gray-200"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Pricing
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/blog"
-              className="flex py-2 text-gray-300 hover:text-gray-200"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link
               to="/about"
               className="flex py-2 text-gray-300 hover:text-gray-200"
               onClick={() => setMobileNavOpen(false)}
             >
-              About us
-            </Link>
-          </li>
-          <li className="my-2 border-b border-t border-gray-700 py-2">
-            <span className="flex py-2 text-gray-300">Support</span>
-            <ul className="pl-4">
-              <li>
-                <Link
-                  to="/contact"
-                  className="flex py-2 text-sm font-medium text-gray-400 hover:text-gray-200"
-                  onClick={() => setMobileNavOpen(false)}
-                >
-                  Contact us
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/help/frequently-asked-questions"
-                  className="flex py-2 text-sm font-medium text-gray-400 hover:text-gray-200"
-                  onClick={() => setMobileNavOpen(false)}
-                >
-                  Help center
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/404"
-                  className="flex py-2 text-sm font-medium text-gray-400 hover:text-gray-200"
-                  onClick={() => setMobileNavOpen(false)}
-                >
-                  404
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link
-              to="/signin"
-              className="flex w-full justify-center py-2 font-medium text-purple-600 hover:text-gray-200"
-              onClick={() => setMobileNavOpen(false)}
-            >
-              Sign in
+              About
             </Link>
           </li>
           <li>
             <Link
-              to="/signup"
-              className="my-2 inline-flex w-full items-center justify-center rounded-sm border border-transparent bg-purple-600 px-4 py-2 font-medium text-white transition duration-150 ease-in-out hover:bg-purple-700"
+              to="/data-route"
+              className="flex py-2 text-gray-300 hover:text-gray-200"
               onClick={() => setMobileNavOpen(false)}
             >
-              Sign up
+              Data Route
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/no-match"
+              className="flex py-2 text-gray-300 hover:text-gray-200"
+              onClick={() => setMobileNavOpen(false)}
+            >
+              No Match
             </Link>
           </li>
         </ul>
